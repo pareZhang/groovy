390dad35a85ec3355beb2054f6572ee10621cf0a
pareZhang <pareZhang@126.com>
2021-02-14 21:07:47 +0800

    Initial commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @Author pareZhang
+ * @Date 2021/2/14 20:45
+ **/
+public class Test {
+    final static String string = "hello";
+
+    public static void main(String[] args) {
+        String str = "hello world";
+        System.out.println(str);
+    }
+}
